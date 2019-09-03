c366812ee8e6bf9dbb57eac832b299f50dc74250
korolpaul <korol.paul@gmail.com>
2019-09-04 00:28:23 +0300

    main page

--- src/js/main.js
+++ src/js/main.js
@@ -29,4 +29,13 @@ $(document).ready(function () {
         create: true,
         sortField: 'text'
     });
+
+    /* Slider */
+    $('.slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        items: 1
+    })
 });
