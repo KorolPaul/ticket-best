6384cd07303f1ba33ecea40e89fbbc1affb14538
korolpaul <korol.paul@gmail.com>
2019-09-07 23:13:54 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -25,9 +25,42 @@ $(document).ready(function () {
     $('.fade').on('click', togglePopup);
 
     /* Dropdowns */
-    $('select').selectize({
+    $('select:not([id])').selectize({
         create: true,
         sortField: 'text'
+    });
+
+    /* Search aotucomplet */
+    $('#select-search').selectize({
+        valueField: 'url',
+        labelField: 'name',
+        searchField: 'name',
+        options: [],
+        create: false,
+        render: {
+            option: function(item, escape) {
+                return '<div>' + escape(item.name) +'</div>';
+            }
+        },
+        score: function(search) {
+            var score = this.getScoreFunction(search);
+            return function(item) {
+                return score(item) * (1 + Math.min(item.watchers / 100, 1));
+            };
+        },
+        load: function(query, callback) {
+            if (!query.length) return callback();
+            $.ajax({
+                url: 'https://api.github.com/legacy/repos/search/' + encodeURIComponent(query),
+                type: 'GET',
+                error: function() {
+                    callback();
+                },
+                success: function(res) {
+                    callback(res.repositories.slice(0, 10));
+                }
+            });
+        }
     });
 
     /* Slider */
@@ -37,6 +70,25 @@ $(document).ready(function () {
         nav: false,
         dots: true,
         items: 1
+    });
+
+    $('.participants').owlCarousel({
+        loop: false,
+        margin: 0,
+        nav: false,
+        dots: true,
+        items: 1,
+        responsive:{
+            1024:{
+                margin: 20,
+                items: 2,
+            },
+            1200:{
+                margin: 20,
+                items: 4,
+                nav: false,
+            }
+        }
     });
 
     $('.reviews').owlCarousel({
